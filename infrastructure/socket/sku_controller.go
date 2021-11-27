e807ac4e6c1b6d1d76dcb741e947e35564e94dd1
fcorrionero <fcorrionero@gmail.com>
2021-11-27 12:35:25 +0100

    wip

--- infrastructure/socket/sku_controller.go
+++ infrastructure/socket/sku_controller.go
@@ -79,5 +79,5 @@ func (controller SkuController) GenerateReport(sessionId string) {
 	query := application.GenerateReportQuery{SessionId: sessionId}
 	reportDto := controller.generateReportQueryHandler.Handle(query)
 
-	log.Println(fmt.Sprintf("Received %d unique product skus, %d duplicates, %d discard values", reportDto.Unique, (reportDto.Received - reportDto.Unique), reportDto.Discarded))
+	log.Println(fmt.Sprintf("Received %d unique product skus, %d duplicates, %d discard values", reportDto.Unique, (reportDto.Received - reportDto.Unique - reportDto.Discarded), reportDto.Discarded))
 }
